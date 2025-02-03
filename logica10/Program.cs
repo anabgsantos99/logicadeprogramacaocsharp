dcc77e640d555f54863960b20ea6905ae5742172
ANA BEATRIZ GOMES DOS SANTOS <ana.bgsantos10@senacsp.edu.br>
2025-02-03 17:04:05 -0300

    03.02_aprendendo_classes

---
+++ logica10/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace logica10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Definindo um array de inteiros 
+
+            int[] numeros = { 5, 12, 3, 8, 7, 10, 15 };
+
+            //Valor que queremos encontrar
+            int valorProcurado = 8;
+
+            // Chamada para a função de pesquisa sequencial 
+            int resultado = PesquisaSequencial(numeros, valorProcurado);
+
+            // Exibe o resultado
+            if (resultado != -1)
+            {
+                Console.WriteLine($"Valor {valorProcurado} encontrado no índice {resultado}");
+            }
+            else
+            {
+                Console.WriteLine($"Valor {valorProcurado} não encontrado");
+            }
+            
+        }
+
+        //Função para realizar a pesquisa sequencial
+        static int PesquisaSequencial(int[] array, int valor)
+        {
+            // Percorrendo o array
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == valor)
+                {
+                    return i; // Retorna o indice onde o valor foi encontrado
+                }
+            }
+
+            return -1; // Retorna -1 se o valor não for encontrado
+        }
+    }
+}
---
+++ logica11/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace logica11
+{
+    //Definição da classe Pessoa
+    internal class Pessoa
+    {
+       //Propriedades da classe
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+
+        //Método para exibir as informações da pessoa
+        public void Apresentar()
+        {
+            Console.WriteLine($"Olá, meu nome é {Nome} e eu tenho {Idade} anos.");
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Criando um objeto da classe Pessoa
+            Pessoa pessoa1 = new Pessoa();
+
+            //Solicitando informações do usuário
+            Console.Write("Digite seu nome: ");
+            pessoa1.Nome = Console.ReadLine();
+
+            Console.Write("Digite sua idade: ");
+            pessoa1.Idade = int.Parse( Console.ReadLine() );
+
+            //Chamando o método para apresentar as informações 
+            pessoa1.Apresentar();
+
+        }
+    }
+}
